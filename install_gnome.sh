42f07eeac05ed62c1d0411ef24afae7be83ffccf
mruniquename <138279642+mruniquename@users.noreply.github.com>
2023-08-31 14:27:15 +0530

    Create install_gnome.sh

---
+++ install_gnome.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Update package repositories
+sudo apt update
+
+# Install GNOME desktop environment
+sudo apt install ubuntu-gnome-desktop -y
+
+# Reboot to apply changes
+sudo reboot
